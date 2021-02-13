67fe9cdd46609768376f67c94ffff5429813c388
TackyLeek <wilsonsecord@gmail.com>
2021-02-13 00:25:08 -0500

    Update calculus.py
    Added comments and added the ability to understand trig functions and plot reliably.

--- calculus.py
+++ calculus.py
@@ -1,20 +1,32 @@
-from sympy import *
-from sympy import symbols
-from sympy.plotting import plot
-from regex import *
+import sympy
+import regex
 
-# 'x ** 3 * x + m + m ** 2'
+# 'x ** 3 * x ** 2'
 ### TURNING INPUT INTO AN EQUATION
 init_term = input("Type your equation: ")
+
+# Adds spaces to help parsing
 init_term = f' {init_term} '
 
-line = findall('\s[a-z]\s', init_term)
-print(line)
-for item in line:
-    init_term = init_term.replace(item, f"symbol('{item}')")
-    line.remove(item)
-    
+# Creates a list of trig functions
+functs = ['sin', 'cos', 'tan', 'csc', 'sec', 'cot']
 
-equation = init_term.replace(" ", "")
-print(equation)
-plot(equation)
+# Sets the size of the x axis in the grid
+size = 10
+
+line = regex.findall('\s[a-z]\s', init_term)
+line = list(dict.fromkeys(line))
+
+for item in line:
+    init_term = init_term.replace(item, f" sympy.symbols('{item}') ")
+    line.remove(item)
+
+for item in functs:
+    init_term = init_term.replace(item, f" sympy.{item}")
+    
+equation = init_term.replace("' ", "'")
+equation = equation.replace(" '", "'")
+# , (sympy.symbols('x'), -size, size)
+p1 = sympy.plot(eval(equation))
+
+p1
